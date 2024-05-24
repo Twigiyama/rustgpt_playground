e566a325076e198ee0f34bb7af6fd37c3f15c07e
Asitha Rodrigo <asitha@mac.com>
2024-05-24 15:47:00 +0100

    Added tests

--- src/myfunc.rs
+++ src/myfunc.rs
@@ -1,3 +1,16 @@
 pub fn add_five(num: u32) -> u32 {
     num + 5
 }
+
+#[cfg(test)]
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn adds_five_test() {
+        let x: u32 = 100;
+        let y: u32 = add_five(x);
+        println!("x and y are from test: {} {}", x, y);
+    }
+}
--- src/other_funcs/minus_funcs.rs
+++ src/other_funcs/minus_funcs.rs
@@ -1,3 +1,18 @@
 pub fn sub_five(num: u32) -> u32 {
     num - 5
 }
+
+
+#[cfg(test)]
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn minus_five_test() {
+        let x: u32 = 100;
+        let y: u32 = sub_five(x);
+        println!("x and y are from test: {} {}", x, y);
+        assert!(y == 95);
+    }
+}
