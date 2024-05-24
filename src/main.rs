c5ae8cc4fa593b0612a0b4c08a8727aecc3ea4b0
Asitha Rodrigo <asitha@mac.com>
2024-05-24 13:24:12 +0100

    Modules

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-fn add_five(x: u32) -> i32 {
-    x + 5
-}
+mod myfunc;
+mod other_funcs;
+
+use crate::myfunc::add_five;
+use crate::other_funcs::minus_funcs::sub_five;
+
+//Everything defaults to immutable
 
 fn main() {
     println!("Hello, world!");
-    print!("balls to Monty");
+    print!("balls to Monty\n");
+
+    let mut x: u32 = 5;
+    println!("X is {}", x);
+
+    let y: u32 = add_five(x);
+    println!("Y is {}", y);
+
+    x = 60;
+
+    println!("X is {}", x);
+
+    let z: u32 = sub_five(x);
+    println!("Z is {}", z);
 }
---
+++ src/other_funcs/minus_funcs.rs
@@ -0,0 +1,3 @@
+pub fn sub_five(num: u32) -> u32 {
+    num - 5
+}
